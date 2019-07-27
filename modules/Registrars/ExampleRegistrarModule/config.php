1afb1549a4ec72fb5830627d6f7b6d364fb1c168
WISECP <50171491+wisecp@users.noreply.github.com>
2019-07-27 16:33:17 +0300

    Update config.php

--- modules/Registrars/ExampleRegistrarModule/config.php
+++ modules/Registrars/ExampleRegistrarModule/config.php
@@ -9,7 +9,7 @@ return [
         'username'          => '',
         'password'          => '',
         'test-mode'         => 0,
-        'whidden-amount'    => 3,
+        'whidden-amount'    => 0,
         'whidden-currency'  => 4,
         'adp'               => false,
         'cost-currency'     => 4,
